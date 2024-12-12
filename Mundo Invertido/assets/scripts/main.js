f7fba952b879b0d2153056bf1625d225aead7813
Fabriciobr5975 <araujofabricio71@gmail.com>
2024-12-11 22:19:07 -0300

    Adicionando alguns comentários

--- Mundo Invertido/assets/scripts/main.js
+++ Mundo Invertido/assets/scripts/main.js
@@ -1,4 +1,7 @@
-function switchTheme(){
+/**
+ * Função para mudar a cor do página quando o botão for pressionado
+ */
+function switchTheme() {
     document.body.classList.toggle('dark-theme');
     document.body.classList.toggle('light-theme');
 }
